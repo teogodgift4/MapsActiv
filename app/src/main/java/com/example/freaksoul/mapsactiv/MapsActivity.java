19d0ca3a62d58fd0d8a88c66b35cce056154fc67
freaksoul <teo.godgift4@gmail.com>
2017-11-21 12:01:42 +0200

    reverse geocoding

--- app/src/main/java/com/example/freaksoul/mapsactiv/MapsActivity.java
+++ app/src/main/java/com/example/freaksoul/mapsactiv/MapsActivity.java
@@ -1,5 +1,14 @@
 package com.example.freaksoul.mapsactiv;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -15,9 +24,16 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, OnMapClickListener {
 
     private GoogleMap mMap;
+    private static final long MINIMUM_DISTANCE_FOR_UPDATES = 1;
+    private static final long MINIMUM_TIME_FOR_UPDATES = 1000;
+    protected LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +59,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
         double lati= point.latitude;
         double longi = point.longitude;
-        Toast.makeText(getBaseContext(), lati+" and "+longi, Toast.LENGTH_LONG).show();
+        Geocoder geoCoder = new Geocoder(getBaseContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = geoCoder.getFromLocation(lati,longi,1);
+            String add="";
+            if(addresses.size()>=0){
+                for(int i=0; i<addresses.get(0).getMaxAddressLineIndex();i++){
+                    add +=addresses.get(0).getAddressLine(i)+"\n";
+                }
+            }
+            Toast.makeText(getBaseContext(), add, Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class MyLocationListener implements LocationListener{
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
     }
 }
